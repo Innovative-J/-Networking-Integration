f6a5cd2ad047ea2a345cb8bfe623183f54fb5ad6
Brooke <brookedunlap97@gmail.com>
2024-04-09 21:10:23 -0500

    Added basic JS functions.

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,48 @@
 console.log('hello world!');
+
+//Function to take user input for login
+
+function login () {
+
+}
+
+
+
+/*When selecting a song, the app should retrieve and display lyrics from MusiXMatch, if available. Create a JS function to get 
+the lyrics from MusiXMatch using their API. If lyrics are not available, the site should indicate that they are not found. */
+
+//root URL https://api.musixmatch.com/ws/1.1/
+
+function getLyrics() {
+
+
+}
+
+function displayLyrics() {
+
+    //if no lyrics then return "Sorry, we were unable to retrieve lyrics for this song."
+
+}
+
+/*The site should provide options to play the selected song on Spotify directly. Need to create function to gather Spotify 
+song audio from Spotify using their API.*/ 
+
+function getAudio() {
+
+}
+
+/*Upon searching, the app should fetch and display relevant results from Spotify. Create a JavaScript function to retrieve 
+the song name and artist name.*/ 
+
+function getSongName() {
+
+}
+
+function getArtistName() {
+
+}
+
+//Handles search functionality
+function handleSearch() {
+
+}
