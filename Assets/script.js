e62ca45e46cb3b3eac59386274d7322571dea43b
Brooke <brookedunlap97@gmail.com>
2024-04-13 21:09:59 -0500

    Added functions for handling local storage. Commented out getLyrics() as it is still a work in progress.

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,18 @@
-console.log('hello world!');
+const mApiKey = '363fa0717b84ce5037dd3d286985b633';
+
+// Function to store data in localStorage if it doesn't already exist
+function storeDataInLocalStorage(key, data) {
+    if (localStorage.getItem(key) === null) {
+        localStorage.setItem(key, JSON.stringify(data));
+    }
+}
+
+// Function to retrieve data from localStorage
+function getDataFromLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
 
 //Function to take user input for login
-
 function login () {
 
 }
@@ -12,11 +23,32 @@ function login () {
 the lyrics from MusiXMatch using their API. If lyrics are not available, the site should indicate that they are not found. */
 
 //root URL https://api.musixmatch.com/ws/1.1/
+/*function getLyrics() {
 
-function getLyrics() {
+const requestURL = `https://api.musixmatch.com/ws/1.1/chart.artists.get?apikey=${mApiKey}&page=1&page_size=3&country=it`; //call for Somebody I used to know
 
-
+fetch (requestURL, { 
+    mode: 'cors',
+    method: 'GET',
+})
+    .then (response => {
+        if (!response.ok) {
+            console.log(response); 
+            return;
+        }
+        return response.json();
+    })
+    /*.catch (error => {
+        console.log('You have an error dumbie');
+        console.log(error.message);
+    })
+    .then (data => {
+        console.log(data);
+    });
 }
+
+getLyrics(); //used to test function 
+*/
 
 function displayLyrics() {
 
