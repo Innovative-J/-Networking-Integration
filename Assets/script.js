4b377d5afac7606ca2c10bc0c3714795c8d3a5d8
Innovative-J <juan.cia05678@gmail.com>
2024-04-15 23:03:50 -0500

    reworked api and pulling data from spotify

--- Assets/script.js
+++ Assets/script.js
@@ -1,75 +1,68 @@
 console.log("hello world!");
 
 // spotify api
-function authenticate() {}
-
-//Function to take user input for login
-
-function login() {
-    // const buffer = new ArrayBuffer()
-    fetch('https://accounts.spotify.com/api/token',{
-        method:'POST',
-        body: `grant_type=client_credentials&client_id= `
-    })
 
 
+async function getToken()  {
+    console.log("Calling getToken!");
+    const headersObj = {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${btoa(
+        `b9bb37e8b2a54a508b6cb8940440243b:cbb8435b6fb84099825819fb12220fc8`
+      ).toString("base64")}`,
+      json: true,
+    };
+    return fetch(
+      "https://accounts.spotify.com/api/token?grant_type=client_credentials",
+      {
+        method: "POST",
+        headers: headersObj,
+      }
+    ).then(function (response) {
+        console.log("Response:", response);
+        return response.json();
+      }).then(function (data) {
+        console.log("Data", data);
+         return result = data.access_token;
+      });
 
-// remake #3 currently fixing the issues with token and uncaught promise.
+  }
+  
+//   getToken();
 
-    const authOption = {
-        headers: {
-            'Authorization': 'Basic' + (btoa('b9bb37e8b2a54a508b6cb8940440243b' + ':' + 'cbb8435b6fb84099825819fb12220fc8').toString('base64'))
-        },
-        form:{
-        grant_type: 'client_credentials'
-        },
-        json: true
+
+
+
+
+// get artist
+async function getArtist(){
+    const token= await getToken();
+    const search = 'artist:Drake';
+    console.log('token', token)
+fetch(`https://api.spotify.com/v1/search?q=${search}&type=artist&local=en-US`, {
+   method:'GET',
+    headers: {
+        'Authorization': `Bearer ${token}`
     }
-    fetch('https://accounts.spotify.com/api/token', authOption,{
-        method: 'POST'
-    }).then(function(repsonse){
-        return repsonse.json();
-        }).then(function(data){
-            console.log(data)
-        })
-
-
-// current api below was a remake of 1 and 2, its there for reference 
-//   const qstring = {
-//     response_type: "code",
-//     client_id: "b9bb37e8b2a54a508b6cb8940440243b",
-//     redirect_uri: "http://localhost:3000/callback",
-//   };
-//   fetch("https://accounts.spotify.com/authorize?" + JSON.stringify(qstring),{
-//     mode:'no-cors'
-//   }).then(function(response){
-//     return response.json();
-//   }).then(function(data){
-//     console.log('spotifyData', data)
-//   });
-
-  // fetch('https://accounts.spotify.com/authorize?', {
-
-  //     // getting info
-  //     headers: {
-  //         //
-  //         'Accept': 'applications/json',
-  //         'Content-Type': 'application/json',
-  //         // 'Access-Control-Allow-Origin': '*',
-  //         'response_type': 'code',
-  //         'client_id':'b9bb37e8b2a54a508b6cb8940440243b',
-  //         'redirect_uri': 'Http://localhost3000',
-
-  //     }
-  // }).then(function (response){
-  //     return response.json()
-  // }).then(function (data){
-  //     console.log('spotify', data)
-  // })
+}).then(function(response){
+    console.log('response',response.json())
+    return response.json()
+}).then(function(data){
+    console.log('data', data)
+    return data 
+})
 }
-login();
+ getArtist()
 
-// .then creating a promise will be excuted
+// create a form that retunts the 
+// repeat the retuns and .then
+
+
+
+
+
+// }
 
 /*When selecting a song, the app should retrieve and display lyrics from MusiXMatch, if available. Create a JS function to get 
 the lyrics from MusiXMatch using their API. If lyrics are not available, the site should indicate that they are not found. */
@@ -90,9 +83,37 @@ function getAudio() {}
 /*Upon searching, the app should fetch and display relevant results from Spotify. Create a JavaScript function to retrieve 
 the song name and artist name.*/
 
-function getSongName() {}
+// Function to search for artists and songs using the Spotify API
+function searchSpotify(query) {
+    // const accessToken = getToken();
 
-function getArtistName() {}
+    // // Encode the query string
+    // const encodedQuery = encodeURIComponent(query);
 
-//Handles search functionality
-function handleSearch() {}
+    // // Spotify API endpoint for searching artists and tracks
+    // const apiUrl = `https://api.spotify.com/v1/search?q=${encodedQuery}&type=artist,track`;
+
+    // // Fetch data from Spotify API
+    // fetch(apiUrl, {
+    //     headers: {
+    //         'Authorization': `Bearer ${accessToken}`
+    //     }
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
+    //     // Example: Display the first artist and track in the console
+    //     if (data.artists && data.artists.items.length > 0) {
+    //         console.log('Artist:', data.artists.items[0]);
+    //     }
+    //     if (data.tracks && data.tracks.items.length > 0) {
+    //         console.log('Track:', data.tracks.items[0]);
+    //     }
+    // })
+    // .catch(error => {
+    //     // displays an error message when fail to fetch
+    //     console.error('Error fetching data:', error);
+    // });
+}
+
+// Example usage:
